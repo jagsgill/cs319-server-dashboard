7d2d2e383a963b5faf57153a565ec74c993a0b14
Eli <eliasjf@britecontent.com>
2016-03-05 11:32:41 -0800

    first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/EliasFriedman/Documents/code/cs319/cs319/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ web_app/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from djangotoolbox.fields import EmbeddedModelField
+
+# Create your models here.
+
+class AccelerometerPayload(models.Model):
+    x = models.FloatField(null=True)
+    y = models.FloatField(null=True)
+    z = models.FloatField(null=True)
+
+class Location(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+class WatchEvent(models.Model):
+    timeStamp = DateTimeField()
+    accelerometerPayload = ListField(EmbeddedModelField('AccelerometerPayload'))
+    location = ListField(EmbeddedModelField('Location'))
+
+class Device(models.Model):
+    deviceId = DecimalField()
+    watchEvents = ListField(EmbeddedModelField('WatchEvent'))
+    
