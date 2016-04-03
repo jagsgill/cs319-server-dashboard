7c420fbd1a1d6d79dd63558efdac9889178f500b
Eli <eliasjf@britecontent.com>
2016-04-03 15:13:39 -0700

    start of dashboard filters

--- web_app/static/web_app/js/dashboard.js
+++ web_app/static/web_app/js/dashboard.js
@@ -1,3 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  console.log("hello world");
 
-});
+  $('input[type=radio][name=device_listing]').change(function() {
+  console.log($(this).val());
+  var fieldToShow = $(this).val();
+  $("#" + fieldToShow).css("display", "block");
+  if (("#" + fieldToShow) == '#all_devices') {
+      $('#all_devices').css("display", "block");
+      $('#online').css("display", "none");
+      $('#offline').css("display", "none");
+  } else if (("#" + fieldToShow) == '#online'){
+    $('#online').css("display", "block");
+    $('#all_devices').css("display", "none");
+    $('#offline').css("display", "none");
+  } else {
+    $('#offline').css("display", "block");
+    $('#all_devices').css("display", "none");
+    $('#online').css("display", "none");
+  }
+  });
+
+})
--- web_app/views.py
+++ web_app/views.py
@@ -2,41 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from django.core.serializers.json import DjangoJSONEncoder
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 
 from models import DataPoint
-
-# serve login page
-def login(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('login.html', c)
-
-
-def authview(request):
-    username = request.POST.get('username','')
-    password = request.POST.get('password','')
-    user = auth.authenticate(username=username,password=password)
-    if user is not None:
-        auth.login(request,user)
-        return HttpResponseRedirect('/dashboard/loggedin')
-    else:
-        return HttpResponseRedirect('/dashboard/invalidlogin')
-
-
-def loggedin(request):
-    return render_to_response('loggedin.html', {'full_name': request.user.username})
-
-
-def invalidlogin(request):
-    return render_to_response('invalidlogin.html')
-
-
-def logout(request):
-    auth.logout(request)
-    return render_to_response('logout.html')
-
 
 # date  range //PO
 def get_device_by_time_range(request, start_time, end_time):
